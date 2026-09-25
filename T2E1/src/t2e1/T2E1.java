8ff12f94f2c0badbb85ef91c03161f62f1e3b1b3
vjp-davidHD <dhernandezd10@informatica.iesvalledeljerteplasencia.es>
2026-09-25 17:27:57 +0200

    prueba1

---
+++ T2E1/src/t2e1/T2E1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t2e1;
+
+/**
+ *
+ * @author alumno
+ */
+public class T2E1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero1 = 30;
+        int numero2;
+        int resultado;
+        numero2 = 700;
+        resultado = numero1 + numero2;
+        System.out.println("El resultado de la suma es " + "= " + resultado);
+        
+    }
+    
+}
