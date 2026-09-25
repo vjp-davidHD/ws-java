5ff22bc04c10836613a82d6feeade05529ac0e9f
vjp-davidHD <dhernandezd10@informatica.iesvalledeljerteplasencia.es>
2026-09-25 18:01:21 +0200

    subida 7

--- Tema2/T2E23/src/t2e23/T2E23.java
+++ Tema2/T2E23/src/t2e23/T2E23.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package t2e23;
-import java.util.Scanner;
+import java.util.Scanner; // importamos escanner
 /**
  *
  * @author alumno
@@ -15,20 +15,23 @@ public class T2E23 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in); // damos nobre entrada al escanner
         
+        // declaramos las variables
         int precio;
         int unidades;
         int total;
         
+        //pedimos datos
         System.out.println("Introduce el precio: "); 
         precio = entrada.nextInt();
         System.out.println("");
         System.out.println("Introduce las unidades: ");
         unidades = entrada.nextInt();
-        
+        // calculamos
         total = precio * unidades;
         
+        //indicamos el precio final
         System.out.println("El precio total es: " + total);
         
         
